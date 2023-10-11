c95f00f0251a85e1291cfe9d9b6a267100522693
jmyv08 <velarde.jm08@gmail.com>
2023-10-11 14:17:28 +0300

    Update country detail page.

--- pages/detail/[country].js
+++ pages/detail/[country].js
@@ -1,17 +1,56 @@
 import { useRouter } from "next/router";
+
+import { FaArrowLeft } from 'react-icons/fa6';
 import Link from "next/link";
 
 export default function CountryDetail({ countriesData }) {
     const router = useRouter();
-    // console.log(router.query);
-    // console.log(countriesData);
+
     let country = countriesData[Number(router.query.country)];
-    
+
 
     return (
         <div>
-            <h1>{country.name}</h1>
-            <Link href="/">{"<<Back"}</Link>
+            <Link href="/"><FaArrowLeft className="inline-block" /> Back</Link>
+
+            <img className='rounded-t ' src={country.flags.svg}></img>
+
+            <h2>{country.name}</h2>
+
+            <div className='py-8'>
+                <p className='mb-0.5'>
+                    <span>Native Name:</span> {country.population}
+                </p>
+                <p className='mb-0.5'>
+                    <span>Population:</span> {country.population}
+                </p>
+                <p className='mb-0.5'>
+                    <span>Region:</span> {country.region}
+                </p>
+                <p className='mb-0.5'>
+                    <span>Sub Region:</span> {country.capital}
+                </p>
+                <p className='mb-0.5'>
+                    <span>Capital:</span> {country.population}
+                </p>
+            </div>
+
+            <div className='py-8'>
+                <p className='mb-0.5'>
+                    <span>Top Level Domain:</span> {country.population}
+                </p>
+                <p className='mb-0.5'>
+                    <span>Currencies:</span> {country.population}
+                </p>
+                <p className='mb-0.5'>
+                    <span>Languages:</span> {country.region}
+                </p>
+            </div>
+
+            <div>
+                <h3>Border Countries:</h3>
+                <div></div>
+            </div>
         </div>
     )
 }
